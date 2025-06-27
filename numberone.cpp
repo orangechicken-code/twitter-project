9996e6434b1705f611c5ab2d8dea9437e4a27650
gahyun <cgh6690@dongyang.ac.kr>
2025-06-27 12:23:37 +0900

    wut

--- numberone.cpp
+++ numberone.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
 struct ownerinfo {
 	char name[20];
-	char phonenumber[13];
+	std::cin >> phonenumber;
 	char email[30];
 	char address[50];
 	char twitterAcount[20];
@@ -15,3 +16,19 @@ struct ContactInfo {
 ownerinfo owner;
 ContactInfo contacts[100];
 int contactnumber = 0;
+
+void inputownerinfo() {
+	std::cout << "?????:";
+	std::cin >> owner.name;
+	
+	std::cout << "??? ????:";
+	std::cin >> owner.phonenumber;
+
+	std::cout << "??? ?? : ";
+	std cin >> owner.address;
+
+	std::cout << "??? ??? ??:";
+	std::cin >> owner.twitterAcount;
+
+	std::cout << "??? ???????." << std::endl;
+}
